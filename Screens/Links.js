8cb0ecb5f54395aa3071ca1a11a67f154478b847
Bhavanisimhadri <bhavani.simhadri1027@gmail.com>
2025-09-05 18:11:45 +0530

    change in image link

--- Screens/Links.js
+++ Screens/Links.js
@@ -1,3 +1,3 @@
 const BASE_URL="https://dikshi.ddns.net/rnworkorder/api/";
-const IMG_URL="https://dikshi.ddns.net/rnworkorder/images/";
+const IMG_URL="https://dikshi.ddns.net/workorder/images/";
 export {BASE_URL,IMG_URL};
