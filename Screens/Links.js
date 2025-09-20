fb852b8798b9a74dc64baef6a8780d602e271105
Bhavanisimhadri <bhavani.simhadri1027@gmail.com>
2025-09-20 16:53:39 +0530

    chnages

--- Screens/Links.js
+++ Screens/Links.js
@@ -1,3 +1,3 @@
-const BASE_URL="https://dikshi.ddns.net/rnworkorder/api/";
-const IMG_URL="https://dikshi.ddns.net/workorder/images/";
+const BASE_URL="https://dikshi.ddns.net/rnworkorderdemo/api/";
+const IMG_URL="https://dikshi.ddns.net/rnworkorderdemo/images/";
 export {BASE_URL,IMG_URL};
