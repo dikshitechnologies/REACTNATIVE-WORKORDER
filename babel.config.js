abbfe9d045115e06d05805e6edd2d2510cb3a29b
Bhavanisimhadri <bhavani.simhadri1027@gmail.com>
2025-10-13 14:21:35 +0530

    changes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
    plugins: [
-    'react-native-reanimated/plugin',  // 👈 must be last
+    // 'react-native-reanimated/plugin',  // 👈 must be last
+    'react-native-worklets/plugin',
   ],
 };
